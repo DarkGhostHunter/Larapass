c565e5da4aa4f930be141b326717ba5908c06618
Viktor Szépe <viktor@szepe.net>
2020-07-11 19:42:21 +0000

    Fix config

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -1,4 +1,9 @@
 <?php
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+config()->set('larapass.relaying_party.name', 'test');
+config()->set('auth.passwords.webauthn', [
+    'provider' => 'users',
+    'table' => 'web_authn_recoveries',
+    'expire' => 60,
+    'throttle' => 60,
+]);
