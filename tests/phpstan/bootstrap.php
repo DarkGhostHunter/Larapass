852e0dc89cd5fbdadd7d246422f75c783c906d02
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2020-07-12 16:31:35 -0400

    Rollback PHPStan until more stable version release.

--- tests/phpstan/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-config()->set('larapass.relaying_party.name', 'test');
-config()->set('auth.passwords.webauthn', [
-    'provider' => 'users',
-    'table' => 'web_authn_recoveries',
-    'expire' => 60,
-    'throttle' => 60,
-]);
